ab2c0f5dd51107069de4da8e30a7d0197f7e8225
Mosey <moseev.es@ya.ru>
2021-01-13 17:49:27 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <br />
+    <button @click="exps++">⬆︎</button>
+    <input v-model="exps" />
+    <Degs :base="2" :exps="new Array(+exps).fill().map((e, i) => i + 10)" />
+  </div>
+</template>
+
+<script>
+import Degs from "./components/Degs.vue";
+
+export default {
+  name: "App",
+  components: {
+    Degs,
+  },
+  data() {
+    return {
+      exps: 3,
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding-left: 100px;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button, input {
+  zoom: 150%;
+}
+</style>
---
+++ src/components/Degs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="exp in exps" :key="exp"><h1><span>{{ base }}</span><sup>{{ exp }}</sup> = {{ base ** exp }}</h1><hr /></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Degs",
+  props: {
+    base: Number,
+    exps: Array,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
